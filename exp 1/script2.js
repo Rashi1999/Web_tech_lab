b2def70ddbc7fc917e6c2e369ab8df01419712a6
Rashi Khandelwal <1999rashi@gmail.com>
2022-02-08 23:34:46 +0530

    Adding exp 2 files

--- exp 1/script2.js
+++ exp 1/script2.js
@@ -10,11 +10,17 @@ var clr_mult_btn=document.getElementById("reset_mul");
 var tabledet=document.querySelector("ul");
 
 function sum(){
-  add.value=parseInt(n1.value)+parseInt(n2.value);
+  if(n1.value.length==0 || n2.value.length ==0)
+    add.value="Please enter number(s) !";
+  else
+    add.value=parseFloat(n1.value)+parseFloat(n2.value);
 }
 
 function subtract(){
-  add.value=parseInt(n1.value)-parseInt(n2.value);
+  if(n1.value.length==0 || n2.value.length ==0)
+    add.value="Please enter number(s) !";
+  else
+    add.value=parseFloat(n1.value)-parseFloat(n2.value);
 }
 
 function clear1() {
@@ -25,16 +31,23 @@ function clear1() {
 
 function cal_table(){
   var tab=[];
-  var n=parseInt(n3.value);
+  var n=parseFloat(n3.value);
   tabledet.innerHTML="";
-  for (var i = 1; i <= 10; i++) {
+  if(n3.value.length==0){
     var pele=document.createElement('li');
-    pele.appendChild(document.createTextNode(i+" x "+n+" = "+n*i));
-    // var x=i.toString().padEnd(7,'\ ')+" x ".toString().padEnd(5," ")+n+" = "+n*i;
-    // console.log(x);
-    // pele.appendChild(document.createTextNode(x));
+    pele.appendChild(document.createTextNode("Please enter a number !"));
     tabledet.appendChild(pele);
   }
+  else{
+    for (var i = 1; i <= 10; i++) {
+      var pele=document.createElement('li');
+      pele.appendChild(document.createTextNode(i+" x "+n+" = "+n*i));
+      // var x=i.toString().padEnd(7,'\ ')+" x ".toString().padEnd(5," ")+n+" = "+n*i;
+      // console.log(x);
+      // pele.appendChild(document.createTextNode(x));
+      tabledet.appendChild(pele);
+    }
+  } 
 }
 
 function clear2() {
